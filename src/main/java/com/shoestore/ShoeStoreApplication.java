31c7c012154635962f310f204a38a4db9bb39edd
Haineee47 <phanhai0407@gmail.com>
2026-07-17 01:14:55 +0700

    TASK-002 Backend Bootstrap

---
+++ src/main/java/com/shoestore/ShoeStoreApplication.java
@@ -0,0 +1,16 @@
+package com.shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application entry point for the Shoe Store backend.
+ */
+@SpringBootApplication
+public class ShoeStoreApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShoeStoreApplication.class, args);
+    }
+}
